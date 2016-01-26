cfecdf805132a241912fec185167f305faa6f80b
Yuan <dengy_1989@126.com>
2016-01-26 21:47:08 +0800

    commit the file to local and remote at the same time

---
+++ src/main/java/com/dy/learn/service/ThirdPrintService.java
@@ -0,0 +1,9 @@
+package com.dy.learn.service;
+
+public class ThirdPrintService {
+	
+	public static void main(String[] args){
+		System.out.println("commit file to local and remote at same time.");
+	}
+
+}
